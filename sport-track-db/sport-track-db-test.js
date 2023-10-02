3a4bf7bcd2fd6a85c606429681c4213525bfc60c
BatLeDev <baptiste.guerny@gmail.com>
2023-10-02 12:48:19 +0200

    Code de test V1

--- sport-track-db/sport-track-db-test.js
+++ sport-track-db/sport-track-db-test.js
@@ -1,7 +1,25 @@
 require ('./sqlite_connection.js')
+const userDao = require('./user_dao');
 
 // Méthode de test
+// Les valeurs de l'utilisateur à ajouter
+const userValues = {
+    email: 'example@example.com',
+    password: 'motdepasse',
+    firstName: 'John',
+    lastName: 'Doe',
+    dateOfBirth: '1990-01-01',
+    gender: 'M',
+    height: 180,
+    weight: 75,
+};
 
-function test() {
-    t = sqlite_connection();
+// Appelez la méthode d'insertion pour ajouter l'utilisateur
+userDao.insert(userValues, (err, userId) => {
+if (err) {
+    console.error('Erreur lors de l\'ajout de l\'utilisateur:', err);
+} else {
+    console.log(`Utilisateur ajouté avec succès, ID: ${userId}`);
 }
+});
+
