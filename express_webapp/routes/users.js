386f25dd3c5c4358cadc48813162cfef587c6dda
Sh3spas <le-chanu.e2202566@etud.univ-ubs.fr>
2023-10-03 17:33:40 +0200

    Views connect + valid pour co et inscription

--- express_webapp/routes/users.js
+++ express_webapp/routes/users.js
@@ -3,8 +3,25 @@ var router = express.Router();
 
 var user_dao = require('sport-track-db').user_dao;
 
+/* Inscription */
 router.get('/add', function(req, res, next) {
     res.render('user_add_form',[]);
 });
+
+router.get('/add/valid', function(req, res, next) {
+    res.render('user_add_valid',[]);
+});
+
+
+/*Connexion*/
+
+router.get('/connect', function(req, res, next) {
+    res.render('user_connect_form',[]);
+});
+
+router.get('/connect/valid', function(req, res, next) {
+    res.render('user_connect_valid',[]);
+});
+
 
 module.exports = router;
