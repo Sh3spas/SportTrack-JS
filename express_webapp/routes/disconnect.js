7cc1a3f7549ebaadeb6eaa92ee46d785d1f081b5
Sh3spas <le-chanu.e2202566@etud.univ-ubs.fr>
2023-10-10 09:51:51 +0200

    disconnect

--- express_webapp/routes/disconnect.js
+++ express_webapp/routes/disconnect.js
@@ -6,6 +6,8 @@ var user_dao = require('sport-track-db').user_dao;
 /* Disconnect */
 
 router.get('/', function(req, res, next) {
+    req.session.destroy();
+    res.locals.session = req.session;
     res.render('user_disconnect',[]);
 });
 
